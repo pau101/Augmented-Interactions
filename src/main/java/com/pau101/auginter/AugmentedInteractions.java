37874c0275f7df04a811841fe2f5c289bc5e2adc
pau101 <paul.fulham0@gmail.com>
2017-04-14 22:35:24 -0700

    Init

---
+++ src/main/java/com/pau101/auginter/AugmentedInteractions.java
@@ -0,0 +1,16 @@
+package com.pau101.auginter;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+	modid = AugmentedInteractions.ID,
+	name = AugmentedInteractions.NAME,
+	version = AugmentedInteractions.VERSION
+)
+public class AugmentedInteractions {
+	public static final String ID = "auginter";
+
+	public static final String NAME = "Augmented Interactions";
+
+	public static final String VERSION = "1.0.0";
+}
