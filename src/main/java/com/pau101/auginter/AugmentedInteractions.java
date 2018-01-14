15f538f37e13635bccd6253768d61a3e7fd11006
Paul Fulham <paul.fulham0@gmail.com>
2018-01-13 16:11:56 -0800

    Release 1.1.0

--- src/main/java/com/pau101/auginter/AugmentedInteractions.java
+++ src/main/java/com/pau101/auginter/AugmentedInteractions.java
@@ -21,7 +21,7 @@ public final class AugmentedInteractions {
 
 	public static final String NAME = "Augmented Interactions";
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 
 	@SidedProxy(
 		clientSide = "com.pau101.auginter.client.ClientProxy",
